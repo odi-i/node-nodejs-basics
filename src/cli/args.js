b9d957fc7ebd43832808784a7ca516da8c12c46a
grom0330 <tokarev-oleg2004@mail.ru>
2024-01-27 14:41:08 +0300

    feat: complete Command line interface task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
+import {argv} from "node:process";
+
+const PREFIX = "--";
+
 const parseArgs = () => {
-    // Write your code here 
+  for (let i = 0; i < argv.length; i = i + 2) {
+    if(argv[i].includes(PREFIX)) console.log(`${argv[i].replace(PREFIX, '')} is ${argv[i + 1]}`)
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+const PREFIX = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+  const allEnv = process.env;
+
+  for (let elem of Object.entries(allEnv)) {
+    if(elem[0].includes(PREFIX)) console.log(`${elem[0]}=${elem[1]}`)
+  }
 };
 
 parseEnv();
