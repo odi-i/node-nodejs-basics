06ab4a9d1044ed13e269466f0716843989ba7440
grom0330 <tokarev-oleg2004@mail.ru>
2024-01-28 15:07:13 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import {createReadStream} from "node:fs";
+import {createHash} from "node:crypto";
+
+const HASH_METHOD = 'sha256'
+const FILE_PATH = new URL('files/fileToCalculateHashFor.txt', import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+  console.log(FILE_PATH)
+
+  const input = createReadStream(FILE_PATH);
+  input.on('readable', () => {
+    const hash = createHash(HASH_METHOD);
+    const data = input.read();
+
+    if (data) {
+      hash.update(data);
+      process.stdout.write(hash.digest('hex'));
+    }
+  })
+}
 
 await calculateHash();
